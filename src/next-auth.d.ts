7a2b111664d738afebcbf2f0fdd232467f63933c
Captainistz <captainistz@gmail.com>
2025-03-24 21:41:48 +0700

    🚀 Implement authentication context and options with next-auth integration

---
+++ src/next-auth.d.ts
@@ -0,0 +1,34 @@
+import { UserRole } from './interfaces/user.interface'
+
+declare module 'next-auth' {
+  interface User {
+    id: string
+    name: string
+    email: string
+    role: UserRole
+    image?: string
+    token: string
+  }
+
+  interface Session {
+    user: {
+      _id: string
+      name: string
+      email: string
+      image?: string
+      role: UserRole
+    }
+    accessToken?: string
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    _id: string
+    name: string
+    email: string
+    role: UserRole
+    image?: string
+    accessToken: string
+  }
+}
