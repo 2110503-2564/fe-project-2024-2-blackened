a02bd842323e754b26adcf8376e763824da6d2be
Captainistz <captainistz@gmail.com>
2025-03-25 11:02:05 +0700

    ✨ Add account settings layout and update form component

--- src/components/user-tag.tsx
+++ src/components/user-tag.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 const UserTag = ({
   logo,
@@ -13,7 +13,9 @@ const UserTag = ({
     <>
       <Avatar className='h-6 w-6 roudned-lg'>
         <AvatarImage src={logo} />
-        <AvatarFallback>FB</AvatarFallback>
+        <AvatarFallback>
+          <div className='size-full bg-muted'></div>
+        </AvatarFallback>
       </Avatar>
       <div className='grid flex-1 text-left text-sm leading-tight'>
         <span className='truncate'>{name}</span>
